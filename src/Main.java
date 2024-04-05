54733582f9a2b12b96f2426e9598da86765d96e4
nurkyzzikirdinova <nzikirdinova7@gmail.com>
2024-04-05 12:47:00 +0600

    Nurkyz created classes and refatoringc

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Nurkyz refactoring");
         System.out.println("Nurkyz refactoring");
+        System.out.println("Nurkyz refactoring");
 
 
 
